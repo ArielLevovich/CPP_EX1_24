80b140662962bb21353820d958f6608356d3f01c
Ariel Levovich <motty13@gmail.com>
2024-05-04 17:39:29 +0300

    Update Graph.hpp

--- Graph.hpp
+++ Graph.hpp
@@ -3,11 +3,19 @@
 #include <iostream>
 
 #pragma once
+using namespace std;
 
 namespace ariel {
     class Graph {
+        private:
+            unsigned int vertices;  
+            vector<vector<int>> adjMatrix;
         public:
             void printGraph();
             void loadGraph(const std::vector<std::vector<int>> &graph);
+            unsigned int getVertices() const;
+            void setVertices(unsigned int v);
+            const vector<vector<int>>& getAdjMatrix() const;
+            void setAdjMatrix(const vector<vector<int>>& matrix);
     };
 }
