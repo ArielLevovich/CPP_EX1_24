cdd6cec82b28abdae49439982f577e8026ae1aa8
Ariel Levovich <motty13@gmail.com>
2024-05-12 02:01:02 +0300

    Update Algorithms.hpp

--- Algorithms.hpp
+++ Algorithms.hpp
@@ -14,15 +14,16 @@ namespace ariel {
             static bool isContainsCycle(const Graph g); 
             static std::string shortestPath(const Graph g, int src, int dest); 
             static std::string isBipartite(const Graph& g); 
-            static bool negativeCycle(const Graph g);             
+            static bool hasNegativeCycle(const Graph g);             
         private:
             static void dfs(const Graph g, int v, vector<bool>& visited);
             static bool detectCycleUtil(const Graph g, unsigned int v, vector<bool>& visited, vector<int>& parent, vector<bool>& recStack); 
             static vector<int> bellmanFord(const Graph g, int src, int end);
             static bool isCyclicUtilUndirected(const Graph g, unsigned int v, bool visited[], int parent, vector<int>& path);
-            static bool isCyclicUndirected(const Graph g);
+            static bool isCyclicUndirected(const Graph g, vector<int>& path);
             static bool isCyclicUtilDirected(const Graph g, unsigned int v, bool visited[], bool* recStack, vector<int>& path);
-            static bool isCyclicDirected(const Graph g);
+            static bool isCyclicDirected(const Graph g, vector<int>& path);
+            static bool isContainsCycleInternal(const Graph g, vector<int>& path);
             static void printCycle(vector<int>& path);
             static void removeAllTheVerticesFromPathTillI(vector<int>& path, unsigned int i);
     };  
