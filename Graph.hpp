2cfc046ecb18879a8ec01fc129e435293c5599fc
Ariel Levovich <motty13@gmail.com>
2024-05-11 21:10:09 +0300

    Update Graph.hpp

--- Graph.hpp
+++ Graph.hpp
@@ -11,14 +11,18 @@ namespace ariel {
             unsigned int vertices;  
             vector<vector<int>> adjMatrix;
             bool isDirectedGraph;
+            bool isNegativeWeight;
         public:
             void printGraph();
             void loadGraph(const std::vector<std::vector<int>> graph);
-            unsigned int getVertices() const;
-            void setVertices(unsigned int v);
-            const vector<vector<int>> getAdjMatrix() const;
+            unsigned int getVertices() const;            
+            const vector<vector<int>> getAdjMatrix() const;            
+            bool isDirected() const;         
+            bool hasNegativeWeight() const;   
+        private:
             void setAdjMatrix(const vector<vector<int>> matrix, bool isDirected);
-            bool isSym(const vector<vector<int>> mtr);
-            bool isDirected() const;
+            bool isSymmetric(const vector<vector<int>> mtr);
+            void setVertices(unsigned int v);            
+            bool hasGraphNegativeWeight(const vector<vector<int>> mtr);
     };
 }
